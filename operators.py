200fed4a58976ded153366f2fdea9bce39263077
prasannag346-tech <prasannag346@gmail.com>
2026-07-29 11:05:11 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,48 @@
+a=10
+b=5
+
+print("addition:",a+b)
+print("subtraction:",a-b)
+print("multiplication:",a*b)
+print("division:",a/b)
+print("modulus division:",a%b)
+print("exponent:",a**b)
+print("floor division:",a//b)
+
+print("\n comparational operators")
+
+a=10
+b=5
+
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a<=b)
+print(a>=b)
+
+print("/n logical operators")
+
+print(10>5 and 10<11)
+print(10==10 and 10>=9)
+print(-10<-11 and -9>-8)
+print(-6.99<6.9 or 5.02==5.0200)
+print(1.960503 or 20==20.00)
+print(10>5 or 10<11)
+print(10==10 and 10>=9)
+print( not(-6.99<6.9 or 5.02==5.0200))
+
+print(10 and 5)
+print(5 and 0)
+print(0 and 5)
+print(0 and 0)
+print(10 and -9)
+print(10 or 5)
+print(5 or 0)
+print(0 or 5)
+print(0 or 0)
+print(10 or -9)
+
+print(5 in [1,2,3,4])
+print(5 not in [1,2,3,4])
+print('c' in "codegnan")
